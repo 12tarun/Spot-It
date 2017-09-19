72e51cf4bf4ef97f795e64d6dbf1f07edaf74501
12tarun <12tarungupta@gmail.com>
2017-09-19 06:44:28 +0530

    Login page modified

--- Login.aspx
+++ Login.aspx
@@ -8,8 +8,25 @@
 </head>
 <body>
       <form id="form1" runat="server">
-      <asp:Login ID = "Login1" runat = "server" OnAuthenticate="ValidateUser" DisplayRememberMe="false"></asp:Login>
-      <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/Registration.aspx">new user register here</asp:HyperLink>
+        <div>
+        <br />
+        Username:<br />
+        <asp:TextBox ID="username" runat="server" Width="200px"></asp:TextBox>
+        <br />
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="please enter a username" ControlToValidate="username" ForeColor="Red"></asp:RequiredFieldValidator>
+        <br />
+        Password:<br />
+        <asp:TextBox ID="password" runat="server" TextMode="Password" Width="200px"></asp:TextBox>
+        <br />
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="password" ErrorMessage="please enter the password" ForeColor="Red"></asp:RequiredFieldValidator>
+        <br />
+        <br />
+        <asp:Button ID="Login_Button" runat="server" OnClick="ValidateUser" Text="Login" Width="100px" />
+        <br />
+        <asp:Label ID="Warning" runat="server"></asp:Label>
+        <br />
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/Registration.aspx">new user register here</asp:HyperLink>
+        </div>
       </form> 
     
 </body>
--- Login.aspx.cs
+++ Login.aspx.cs
@@ -16,26 +16,29 @@ public partial class Login : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        if(!IsPostBack)
+        {
+            Warning.Visible = false;
+        }
         if(Session["LoggedIn"] != null)
         {
             Response.Redirect("Home.aspx");
         }
     }
-
-    protected void ValidateUser(object sender, AuthenticateEventArgs e)
+    protected void ValidateUser(object sender, EventArgs e)
     {
         int userId = 0;
         MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
         Byte[] hashedBytes;
         UTF8Encoding encoder = new UTF8Encoding();
-        hashedBytes = md5Hasher.ComputeHash(encoder.GetBytes(Login1.Password));
+        hashedBytes = md5Hasher.ComputeHash(encoder.GetBytes(password.Text.Trim()));
         string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
         using (SqlConnection con = new SqlConnection(constr))
         {
             using (SqlCommand cmd = new SqlCommand("Validate_User"))
             {
                 cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Username", Login1.UserName);
+                cmd.Parameters.AddWithValue("@Username", username.Text.Trim());
                 cmd.Parameters.AddWithValue("@Password", hashedBytes);
                 cmd.Connection = con;
                 con.Open();
@@ -45,10 +48,14 @@ public partial class Login : System.Web.UI.Page
             switch (userId)
             {
                 case -1:
-                    Login1.FailureText = "Username and/or password is incorrect.";
+                    Warning.Visible = true;
+                    Warning.Text = "Username and/or password is incorrect.";
+                    Warning.ForeColor = System.Drawing.Color.Red;
                     break;
                 case -2:
-                    Login1.FailureText = "Account has not been activated.";
+                    Warning.Visible = true;
+                    Warning.Text = "Account has not been activated.";
+                    Warning.ForeColor = System.Drawing.Color.Red;
                     break;
                 default:
                     Session["LoggedIn"] = userId;
